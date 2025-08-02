fab7dbe25ea70e3f6144baaa7cfb6965242fda37
circle0802 <hawon9781@gmail.com>
2025-08-02 20:40:52 +0900

    logoView 및 authTextField 수정

---
+++ Boo!/Boo!/DesignSystem/AuthTextField.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct AuthTextField: View {
+    var placeholder: String
+    @Binding var text: String
+    var isSecure: Bool = false
+
+    var body: some View {
+        ZStack(alignment: .leading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(.whiteBoo.opacity(0.3))
+                    .font(.pretendard(.medium, size: 14))
+                    .padding(.horizontal, 20)
+            }
+
+            Group {
+                if isSecure {
+                    SecureField("", text: $text)
+                } else {
+                    TextField("", text: $text)
+                }
+            }
+            .frame(height: 45)
+            .padding(.horizontal, 20)
+            .background(.whiteBoo.opacity(0.1))
+            .cornerRadius(100)
+            .font(.pretendard(.medium, size: 14))
+        }
+        .padding(.horizontal, 20)
+    }
+}
+
+#Preview {
+    @Previewable @State var sampleText = ""
+    return AuthTextField(placeholder: "아이디", text: $sampleText)
+}
--- Boo!/Boo!/DesignSystem/logoView.swift
+++ Boo!/Boo!/DesignSystem/logoView.swift
@@ -3,8 +3,11 @@ import SwiftUI
 struct logoView: View {
     var body: some View {
         VStack {
-            Image("logo")
-                .padding(20)
+            HStack(spacing: 10) {
+                Image("logo")
+                Image("logoText")
+            }
+            .padding(18)
             Text("당신만의 공포 해소 챗봇, Boo!")
                 .font(.pretendard(.bold, size: 16))
                 .foregroundStyle(.white)
